87b78f50089c7d61761545936c27e58f04e340df
Melissa Kline Struhl <mekline@mekline.local>
2021-12-29 14:02:56 -0500

    magical bird screenshots

--- get_bird_img/get_birds.py
+++ get_bird_img/get_birds.py
@@ -1,9 +1,39 @@
-from Selenium import webdriver
-
-
-# from Pillow import Image
-
+# Load packages
+from selenium import webdriver
+from PIL import Image
 import sys
-print(sys.version)
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
 
-print("hello")
+# Get list of bird codes from our selection process (data_ebirds)
+birds = pd.read_csv("data_ebird/common_birds_in_USA.csv")
+
+#Make little for testing!
+birds = birds[1:10]
+
+# Set up Chrome driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+for bird in birds.species_code:
+    # Open website window 
+    driver.get("https://ebird.org/species/" + bird)
+    driver.maximize_window()
+    time.sleep(1)
+
+    # Save full screenshot
+    driver.save_screenshot('img/' + bird + '.png')
+
+    # Crop to image!
+    image = Image.open('img/' + bird + '.png')
+
+    # Code smells - precise location of the main image on the ebirds page
+    left = 685
+    top = 115
+    right = left + 860
+    bottom = top + 620
+
+    image = image.crop((left, top, right, bottom)) # defines crop points
+    image.save('img/' + bird + '.png')
+    image.show()
