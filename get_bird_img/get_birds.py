dc3cb5557473d500224b32446dcadc7ac7e59c5c
Melissa Kline Struhl <mekline@mekline.local>
2022-01-01 17:23:47 -0500

    selected ebirds!

--- get_bird_img/get_birds.py
+++ get_bird_img/get_birds.py
@@ -17,23 +17,36 @@ birds = birds[1:10]
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 for bird in birds.species_code:
-    # Open website window 
+    # Open website window and click to first image!
     driver.get("https://ebird.org/species/" + bird)
+    showcase = driver.find_element_by_id("MediaFeed-showcase")
+    showcase.click()
     driver.maximize_window()
-    time.sleep(1)
 
-    # Save full screenshot
-    driver.save_screenshot('img/' + bird + '.png')
+    # Cycle thrugh images!
+    for i in range(3):
+        if i > 0:
+            nextbutton = driver.find_element_by_css_selector("div.Lightbox-nav button:last-of-type")
+            nextbutton.click()
+        
+        # Wait for load and take screenshot
+        time.sleep(1)
+        driver.save_screenshot('img/' + bird + '_' + str(i) + '.png')
 
-    # Crop to image!
-    image = Image.open('img/' + bird + '.png')
 
-    # Code smells - precise location of the main image on the ebirds page
-    left = 685
-    top = 115
-    right = left + 860
-    bottom = top + 620
+        # Reopen image to edit
+        image = Image.open('img/' + bird + '_' + str(i) + '.png')
 
-    image = image.crop((left, top, right, bottom)) # defines crop points
-    image.save('img/' + bird + '.png')
-    image.show()
+        # Code smells - precise location of the main image on the ebirds page
+        left = 425
+        top = 0
+        right = left + 1070
+        bottom = top + 805
+
+        image = image.crop((left, top, right, bottom)) # defines crop points
+        image.save('img/' + bird + '_' + str(i) + '.png')
+        image.show()
+
+
+# Library of snow goose images
+# https://macaulaylibrary.org/catalog?searchField=species&taxonCode=snogoo
